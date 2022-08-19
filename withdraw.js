08ff480adc01349edd4148a7ef3a5bbc04635d0a
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-08-19 06:31:58 -0700

    withdraw update

--- withdraw.js
+++ withdraw.js
@@ -9,24 +9,34 @@ const btnWithdraw = document.getElementById('btn-withdraw').addEventListener('cl
         alert("amount must be filled out");
         return false;
     }
-    // if (inputWithdrawField > withdrawTotal) {
-    //     alert("apnar account a balance nei");
-    //     return false;
-    // }
 
-    const withdrawAmount = document.getElementById('withdraw-amount');
-    const priviuswithdrawAmountString = withdrawAmount.innerText;
-    const priviuswithdrawAmount = parseFloat(priviuswithdrawAmountString);
-    // const totalWithdraw = inputWithdrawField + priviuswithdrawAmount;
-    const withdrawTotal = inputWithdrawField + priviuswithdrawAmount;
-    withdrawAmount.innerText = withdrawTotal;
+
+    inputWithdraw.value = '';
 
     // balance field
     const carrentBalance = document.getElementById('carrent-balance');
     const previusCarrentBalanceString = carrentBalance.innerText;
     const previusCarrentBalance = parseFloat(previusCarrentBalanceString);
+
+
+    if (inputWithdrawFieldString > previusCarrentBalance) {
+        alert("apnar account a balance nei");
+        return;
+    }
+
     const totalBalance = previusCarrentBalance - inputWithdrawField;
     carrentBalance.innerText = totalBalance;
+
+    const withdrawAmount = document.getElementById('withdraw-amount');
+    const priviuswithdrawAmountString = withdrawAmount.innerText;
+    const priviuswithdrawAmount = parseFloat(priviuswithdrawAmountString);
+
+
+    // const totalWithdraw = inputWithdrawField + priviuswithdrawAmount;
+    const withdrawTotal = inputWithdrawField + priviuswithdrawAmount;
+    withdrawAmount.innerText = withdrawTotal;
+
+
     inputWithdraw.value = '';
 
 })
